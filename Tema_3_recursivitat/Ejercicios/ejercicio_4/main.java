aa84df211e89ffaef61d06fc745c31a13d85e914
NicoRuedaA <nico99rueda@gmail.com>
2026-01-23 16:34:24 +0100

    recursividad primo perfecto

---
+++ Tema_3_recursivitat/Ejercicios/ejercicio_4/main.java
@@ -0,0 +1,56 @@
+import java.math.*;
+
+import java.util.Scanner;
+
+public class main {
+
+    public static boolean esPrimeroPerfecto(int x) {
+        if (x == 0) {
+            return true;
+        }
+
+        else if (esPrimo(x)) {
+            return esPrimo(sumDigits(x));
+        } else
+            return false;
+    }
+
+    public static int sumDigits(int x) {
+        if (x < 10)
+            return x;
+
+        else
+
+            return sumDigits(x / 10 + x % 10);
+
+    }
+
+    private static boolean esPrimoIterativo(int a, int b) {
+        if (b == 1) {
+            return true;
+        } else {
+            if (a % b == 0)
+                return false;
+            else
+                return esPrimoIterativo(a, b - 1);
+        }
+    }
+
+    private static boolean esPrimo(int x) {
+        boolean esPrimo = true;
+
+        int y = (int) Math.sqrt(x);
+
+        esPrimo = esPrimoIterativo(x, y);
+        return esPrimo;
+    }
+
+    public static void main(String[] args) {
+        // int x = Integer.parseInt(args[0]);
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(esPrimeroPerfecto(a));
+        sc.close();
+        // 978
+    }
+}
